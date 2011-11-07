774795b3fca88f83412e15d6fee446c1dd274bd1
lauri jauhiainen <rabidus@uhiainen.dyndns.org>
2011-11-07 22:33:41 +0200

    simple UI for irc webviewer

--- www/index.php
+++ www/index.php
@@ -1,5 +1,5 @@
 <?php
-
+header('Content-Type: text/html; charset=utf-8');
 
 ?>
 <html>
@@ -8,18 +8,45 @@
 <script src="http://code.jquery.com/jquery-1.7.min.js"></script>
 <script>
 var last_data = 0;
+var timeout = 5000;
 
 function fetch_new_datas() {
-jQuery.post('ajax/ajax_get_lines.php', { t:last_data }, function(data) {
-		
+	jQuery.post('ajax/ajax_get_lines.php', { t:last_data }, function(data) {
+		json = JSON.parse(data);
+		if(json.error !== undefined) {
+			alert(json.error);
+		} 
+		if(json.lines > 0) {
+//			jQuery("#content").append("Yes new lines<br>");
+			for(var i = 0; i < json.lines; i++) {
+				jQuery("#content").append(json.data[i]+ "<br>");
+			}
+			last_data = json.time;
+			timeout = 5000;
+
+			var obj = document.getElementById('content');
+			obj.scrollTop = obj.scrollHeight;
+		} else {
+			timeout = timeout*1.1;
+			timeout = Math.min(600000, timeout);
+//			jQuery("#content").append("no new data<br>");
+		}
 
 	});
-
+	setTimeout("fetch_new_datas()", timeout);
 }
 
-setInterval("fetch_new_datas()", 5000)
+fetch_new_datas();
 
 </script>
+<style>
+#content {
+	border: 1px solid black;
+	height:99%;
+	width: 99%;
+	overflow: auto;
+}
+</style>
 </head>
 <body>
 <div id="content"></div>
