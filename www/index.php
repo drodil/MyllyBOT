400c481f922cf8f55587c322f2dc542620770e48
lauri jauhiainen <lauri.jauhiainen@gmail.com>
2011-11-07 20:04:24 +0200

    early ajax

--- www/index.php
+++ www/index.php
@@ -1,4 +1,33 @@
 <?php
-echo 'just testing';
+
+
+?>
+<html>
+<head>
+<title>Myllybot backlogist</title>
+<script src="http://code.jquery.com/jquery-1.7.min.js"></script>
+<script>
+var last_data = 0;
+
+function fetch_new_datas() {
+jQuery.post('ajax/ajax_get_lines.php', { t:last_data }, function(data) {
+		
+
+	});
+
+}
+
+setInterval("fetch_new_datas()", 5000)
+
+</script>
+</head>
+<body>
+<div id="content"></div>
+</body>
+</html>
+
+<?php
+
+
 
 ?>
